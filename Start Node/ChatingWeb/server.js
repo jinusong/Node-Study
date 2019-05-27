a43c924b0a85d0b0b82fb0f3c04ef7334fcbdd81
jinusong <sugwer7464@gmail.com>
2019-05-27 10:13:37 +0900

    [Update] ChatingWeb

--- Start Node/ChatingWeb/server.js
+++ Start Node/ChatingWeb/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app); 
 var io = require('socket.io')(http);    
-var path = require('path'); 
+var path = require('path');
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -15,18 +15,22 @@ app.get('/', (req, res) => {
 });
 
 var count=1;
-io.on('connection', function(socket){ 
+io.on('connection', function(socket){   // 채팅방에 접속했을 때 - 1
   	console.log('user connected: ', socket.id);  
   	var name = "익명" + count++;                 
 	socket.name = name;
   	io.to(socket.id).emit('create name', name);   
+	io.emit('new_connect', name);
 	
-	socket.on('disconnect', function(){ 
+	socket.on('disconnect', function(){   // 채팅방 접속이 끊어졌을 때 - 2
 	  console.log('user disconnected: '+ socket.id + ' ' + socket.name);
+	  io.emit('new_disconnect', socket.name);
 	});
 
-	socket.on('send message', function(name, text){ 
+	socket.on('send message', function(name, text){   // 메세지를 보냈을 때 - 3
 		var msg = name + ' : ' + text;
+		if(name != socket.name)   // 닉네임을 바꿨을 때 
+			io.emit('change name', socket.name, name);
 		socket.name = name;
     	console.log(msg);
     	io.emit('receive message', msg);
